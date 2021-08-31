c128ee01625c71d9bc4f7484ad70c0be11791a09
Ahmed-msayed <ahmed.dev@icloud.com>
2021-08-31 16:52:49 +0200

    31-8 passing data and protocol technique

--- StarTrivia/Controller/HomeworldVC.swift
+++ StarTrivia/Controller/HomeworldVC.swift
@@ -9,21 +9,10 @@ import UIKit
 
 class HomeworldVC: UIViewController {
 
+    var personPassed: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(personPassed.name)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- StarTrivia/Controller/SelectPersonVC.swift
+++ StarTrivia/Controller/SelectPersonVC.swift
@@ -22,6 +22,8 @@ class SelectPersonVC: UIViewController {
     
     var personAPI = PersonAPI()
     
+    var personToPass: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -32,6 +34,7 @@ class SelectPersonVC: UIViewController {
         personAPI.getRandomPersonAlamofire(id: randomId) { (person) in
             if let person = person {
                 self.setupView(person: person)
+                self.personToPass = person
             }
         }
     }
@@ -50,26 +53,54 @@ class SelectPersonVC: UIViewController {
         filmsBtn.isEnabled = !person.films.isEmpty
     }
     
+    
     @IBAction func homeworldClicked(_ sender: Any) {
         if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeworldVC") as? HomeworldVC {
             self.navigationController?.pushViewController(viewController, animated: true)
+            viewController.personPassed = personToPass
         }
     }
     @IBAction func vehiclesClicked(_ sender: Any) {
         if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VehiclesVC") as? VehiclesVC {
             self.navigationController?.pushViewController(viewController, animated: true)
+            viewController.personPassed = personToPass
         }
     }
     @IBAction func starshipsClicked(_ sender: Any) {
         if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "StarshipsVC") as? StarshipsVC {
             self.navigationController?.pushViewController(viewController, animated: true)
+            viewController.personPassed = personToPass
         }
     }
     @IBAction func filmsClicked(_ sender: Any) {
         if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "FilmsVC") as? FilmsVC {
             self.navigationController?.pushViewController(viewController, animated: true)
+            viewController.personPassed = personToPass
         }
     }
     
+    
+    /*
+     //if using segues, use prepare for segue with if and else if for each VC
+     
+     //also we van use Protocols with prepare for segue :
+     
+     
+     var personToPass: Person!              //global var
+     
+     self.personToPass = person             //inside random btn
+     
+     protocol PersonProtocol {              //at the end of the VC
+     var person: Person! {get set}
+     }
+     
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if var destination = segue.destination as? PersonProtocol {
+            destination.person = person
+        }
+     }
+     
+     //then conform all the view controllers to the Protocol
+     */
 }
 
